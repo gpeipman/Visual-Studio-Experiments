94aefab30cfa44cfa858e82ab70bb112c9b30201
GunnarPeipman <gunnar@dt.ee>
2010-09-23 17:35:54 +0300

    first commit, don't use, changes are not tested

---
+++ Experiments.SongRequestWorkflow/Experiment.SongRequestWorkflow/PlaylistClient.cs
@@ -0,0 +1,20 @@
+﻿namespace Experiment.SongRequestWorkflow
+{
+    public class PlaylistClient
+    {
+        public static bool SongInQue(string artist, string song)
+        {
+            return false;
+        }
+
+        public static bool SongExists(string artist, string song)
+        {
+            return true;
+        }
+
+        public static void AddToQueue(string artist, string song)
+        {
+
+        }
+    }
+}
