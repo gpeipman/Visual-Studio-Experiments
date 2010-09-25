c3e9c321ba553605c70049e17e1671edf64d0e98
GunnarPeipman <gunnar@dt.ee>
2010-09-25 13:05:47 +0300

    added Vcard example solution, examples about using vCards are under construction

---
+++ Experiments.Vcard/Experiments.Vcard/Vcard.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Experiments.Vcard
+{
+    public class Vcard
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Organization { get; set; }
+        public string JobTitle { get; set; }
+        public string StreetAddress { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string CountryName { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string HomePage { get; set; }
+        public byte[] Image { get; set; }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("BEGIN:VCARD");
+            builder.AppendLine("VERSION:2.1");
+
+            // Name
+            builder.AppendLine("N:" + LastName + ";" + FirstName);
+
+            // Full name
+            builder.AppendLine("FN:" + FirstName + " " + LastName);
+
+            // Address
+            builder.Append("ADR;HOME;PREF:;;");
+            builder.Append(StreetAddress + ";");
+            builder.Append(City + ";;");
+            builder.Append(Zip + ";");
+            builder.AppendLine(CountryName);
+
+            // Other data
+            builder.AppendLine("ORG:" + Organization);
+            builder.AppendLine("TITLE:" + JobTitle);
+            builder.AppendLine("TEL;HOME;VOICE:" + Phone);
+            builder.AppendLine("TEL;CELL;VOICE:" + Mobile);
+            builder.AppendLine("URL;" + HomePage);
+            builder.AppendLine("EMAIL;PREF;INTERNET:" + Email);
+
+            builder.AppendLine("END:VCARD");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Experiments.Vcard/MvcVcardControllerAction/VcardResult.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using Experiments.Vcard;
+
+namespace MvcVcardControllerAction
+{
+    public class VcardResult : ActionResult
+    {
+        private readonly Vcard _card;
+
+        protected VcardResult() { }
+
+        public VcardResult(Vcard card)
+        {
+            _card = card;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "text/vcard";
+            response.AddHeader("Content-Disposition", "attachment; fileName=" + _card.FirstName + " " + _card.LastName + ".vcf");
+
+            var cardString = _card.ToString();
+            var inputEncoding = Encoding.Default;
+            var outputEncoding = Encoding.GetEncoding("windows-1257");
+            var cardBytes = inputEncoding.GetBytes(cardString);
+
+            var outputBytes = Encoding.Convert(inputEncoding,
+                                    outputEncoding, cardBytes);
+
+            response.OutputStream.Write(outputBytes, 0, outputBytes.Length);
+        }
+    }
+}
