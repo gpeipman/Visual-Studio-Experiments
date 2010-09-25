9a01c3ed8b01f66c90beca2f17ce9036faa85fc5
GunnarPeipman <gunnar@dt.ee>
2010-09-25 22:28:55 +0300

    added memory mapped files example to repository

---
+++ Experiments.MemoryMappedFiles/Experiments.MemoryMappedFiles.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace Experiments.MemoryMappedFiles.Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Memory mapped file reader started");
+
+            using (var file = MemoryMappedFile.OpenExisting("myFile"))
+            {
+                using (var reader = file.CreateViewAccessor(0, 24))
+                {
+                    var bytes = new byte[24];
+                    reader.ReadArray(0, bytes, 0, bytes.Length);
+
+                    Console.WriteLine("Reading bytes");
+                    foreach (var t in bytes)
+                        Console.Write((char)t + " ");
+
+                    Console.WriteLine(string.Empty);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Experiments.MemoryMappedFiles/Experiments.MemoryMappedFiles.Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace Experiments.MemoryMappedFiles.Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Memory mapped file server started");
+
+            using (var file = MemoryMappedFile.CreateNew("myFile", int.MaxValue))
+            {
+                var bytes = new byte[24];
+                for (var i = 0; i < bytes.Length; i++)
+                    bytes[i] = (byte)(65 + i);
+
+                using (var writer = file.CreateViewAccessor(0, bytes.Length))
+                {
+                    writer.WriteArray<byte>(0, bytes, 0, bytes.Length);
+                }
+
+                Console.WriteLine("Before exiting run memory mapped file reader");
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
