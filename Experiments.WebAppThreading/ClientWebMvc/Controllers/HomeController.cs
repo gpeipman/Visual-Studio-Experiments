8ffe2ef15de0781c011fb72aa26f0b4ca507ec8c
GunnarPeipman <gunnar@dt.ee>
2010-09-25 03:40:41 +0300

    added ASP.NET MVC asynchronous calls sample project to solution

---
+++ Experiments.WebAppThreading/ClientWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Mvc;
+using ClientWebMvc.ServiceReference1;
+
+namespace ClientWebMvc.Controllers
+{
+    [HandleError]
+    public class HomeController : AsyncController
+    {
+        public void IndexAsync()
+        {            
+            var random = new Random();
+
+            for (var i = 0; i < 4; i++)
+            {
+                AsyncManager.OutstandingOperations.Increment();
+
+                var delay = random.Next(1, 5) * 1000;
+                var service = new DelayedHelloSoapClient();
+                service.HelloWorldCompleted += HelloWorldCompleted;
+                service.HelloWorldAsync(delay);
+
+                Debug.WriteLine("Started: " + service.GetHashCode());
+            }
+        }
+
+        public ActionResult IndexCompleted(List<string> result)
+        {
+            return View(result);
+        }
+    
+        void HelloWorldCompleted(object sender, HelloWorldCompletedEventArgs e)
+        {            
+            var hash = 0;
+            var service = e.UserState as DelayedHelloSoapClient;
+            if (service != null)
+                hash = service.GetHashCode();
+
+            List<string> list;
+            if (AsyncManager.Parameters.ContainsKey("result"))
+            {
+                list = (List<string>) AsyncManager.Parameters["result"];
+            }
+            else
+            {
+                list = new List<string>();
+                AsyncManager.Parameters["result"] = list;
+            }
+
+            list.Add(e.Result);
+
+            Debug.WriteLine("Finished: " + hash + " " + e.Result);
+            AsyncManager.OutstandingOperations.Decrement();            
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Experiments.WebAppThreading/ClientWebMvc/Views/Home/Index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IList<string>>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Home Page
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: ViewData["Message"] %></h2>
+    <p>
+        <% foreach(var item in Model)
+           {
+               Response.Write(item);
+               Response.Write("<br/>");
+           } 
+        %>
+    </p>
+</asp:Content>
