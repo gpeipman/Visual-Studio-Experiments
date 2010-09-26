cb8508d6151ef698119cdf9aa28bb628da4f4aa3
GunnarPeipman <gunnar@dt.ee>
2010-09-26 13:41:31 +0300

    added sample solution that demonstrates performance of two methods for calculating Fibonacci numbers

---
+++ Experiments.FibonacciPerformance/Experiments.FibonacciPerformance/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿namespace Experiments.FibonacciPerformance
+{
+    static class Fibonacci
+    {
+        public static int FibonacciRecursive(int x)
+        {
+            if (x == 0 || x == 1)
+                return x;
+
+            return FibonacciRecursive(x - 1) +
+                   FibonacciRecursive(x - 2);
+        }
+
+        public static int FibonacciFlat(int x)
+        {
+            var previousValue = -1;
+            var currentResult = 1;
+
+            for (var i = 0; i <= x; ++i)
+            {
+                var sum = currentResult + previousValue;
+                previousValue = currentResult;
+                currentResult = sum;
+            }
+
+            return currentResult;
+        }
+    }
+}
---
+++ Experiments.FibonacciPerformance/Experiments.FibonacciPerformance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Experiments.FibonacciPerformance
+{
+    class Program
+    {
+        static void Main()
+        {
+            var watch = new Stopwatch();
+
+            Console.WriteLine("Measuring recursive implementation");
+
+            watch.Start();            
+            for(var i=0; i<42; i++)
+            {
+                Fibonacci.FibonacciRecursive(i);
+            }            
+            watch.Stop();
+
+            Console.WriteLine("Recursive: " + watch.Elapsed);
+            Console.WriteLine("\r\nMeasuring flat implementation");
+
+            watch.Reset();
+            for (var i = 0; i < 42; i++)
+            {
+                Fibonacci.FibonacciFlat(i);
+            }
+            watch.Stop();
+
+            Console.WriteLine("Flat: " + watch.Elapsed);
+            Console.WriteLine("\r\nPress any key to quit ...");
+            Console.ReadLine();
+        }
+    }
+}
