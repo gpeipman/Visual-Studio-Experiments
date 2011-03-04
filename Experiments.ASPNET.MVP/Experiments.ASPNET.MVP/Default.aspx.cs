779c4296fcffbf3f40103980747490ca12bafc35
unknown <GunnarPeipman@.(none)>
2011-03-04 05:48:38 +0200

    added mvp and nunit libraries and test project

--- Experiments.ASPNET.MVP/Experiments.ASPNET.MVP/Default.aspx.cs
+++ Experiments.ASPNET.MVP/Experiments.ASPNET.MVP/Default.aspx.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebFormsMvp.Web;
 
 namespace Experiments.ASPNET.MVP
 {
-    public partial class _Default : System.Web.UI.Page
+    public partial class _Default : MvpPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
